edc1750b8f279402454c56ab98d6e4435b80786b
Cintha Aryani <65070310+CinthaAryani@users.noreply.github.com>
2020-07-18 11:16:48 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+mysql_connect("localhost","root","");
+mysql_select_db("chijab");
+$db['default']= array(
+    'dsn'=>'',
+    'hostname' =>'sql206.epizy.com',
+    'username'=>'epiz_26290372',
+    'password'=>'o3Qxn0QgHymu
+',
+    'database'=>'epiz_26290372_chijab',
+    'dbdriver'=>'mysql',
+    'pconnect'=>false,
+    'db_debug'=> (environment !=='production'),
+    'cache_on'=>false,
+    'cachedir'=>'',
+    'char_set'=>'utf8',
+    'dbcollat'=>'utf8_general_c',
+    'swap_pre'=> '',
+    'encrypt'=>false,
+    'compress'=>false,
+    'stricton'=>true,
+    'failover'=> array(),
+    'save_queries'=>true,
+?>
